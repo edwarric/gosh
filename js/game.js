3701892486d8d0d370c1608fed70dfacdea78b60
Médi-Rémi Hashim <mediremi@gmail.com>
2018-11-01 17:38:46 +0000

    Refactored

---
+++ js/game.js
@@ -0,0 +1,366 @@
+window.components = {}
+
+window.App = {
+    size: {
+        width: 850,
+        height: 900,
+    },
+
+    room: {
+        width: 700,
+        height: 600,
+    },
+
+    load_sprites() {
+        // Load buttons
+        Crafty.sprite("pics/buttons/book-button.png", {
+            bookButton: [0, 0, 78, 78]
+        });
+        Crafty.sprite("pics/buttons/heart-button.png", {
+            heartButton: [0, 0, 78, 78]
+        });
+        Crafty.sprite("pics/buttons/medicine-button.png", {
+            medicineButton: [0, 0, 78, 78]
+        });
+        Crafty.sprite("pics/buttons/sleep-button.png", {
+            sleepButton: [0, 0, 78, 78]
+        });
+        Crafty.sprite("pics/buttons/speech-button.png", {
+            speechButton: [0, 0, 78, 78]
+        });
+        Crafty.sprite("pics/buttons/temp-button.png", {
+            tempButton: [0, 0, 78, 78]
+        });
+
+        // Load icons
+        Crafty.sprite("pics/icons/alert.png", {
+            alert: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/book.png", {
+            reading: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/good.png", {
+            good: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/heart.png", {
+            heart: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/medicine.png", {
+            medicine: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/sleep.png", {
+            sleep: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/speech.png", {
+            speech: [0, 0, 128, 128]
+        });
+        Crafty.sprite("pics/icons/temp.png", {
+            temp: [0, 0, 128, 128]
+        });
+
+        // Load pictures
+        Crafty.sprite("pics/img/button-bg.png", {
+            buttonBg: [0, 0, 645, 150]
+        });
+        Crafty.sprite("pics/img/chair.png", {
+            chair: [0, 0, 60, 60]
+        });
+        Crafty.sprite("pics/img/desk.png", {
+            desk: [0, 0, 60, 60]
+        });
+        Crafty.sprite("pics/img/hallway-side.png", {
+            hallwaySide: [0, 0, 700, 150]
+        });
+        Crafty.sprite("pics/img/hallway-up.png", {
+            hallwayUp: [0, 0, 150, 750]
+        });
+        Crafty.sprite("pics/img/nurse.png", {
+            nurse: [0, 0, 60, 60]
+        });
+        Crafty.sprite("pics/img/wall.png", {
+            wall: [0, 0, 700, 600]
+        });
+
+        // Sprite animations
+        const assets = {
+            "sprites": {
+                "pics/animations/ac.png": {
+                    tile: 90,
+                    tileh: 80,
+                    map: {
+                        acOff: [0, 0],
+                        acOn1: [0, 1],
+                        acOn2: [0, 2]
+                    }
+                },
+                "pics/animations/bed.png": {
+                    tile: 285,
+                    tileh: 130,
+                    map: {
+                        bedOff: [0, 0],
+                        bedOn: [0, 1]
+                    }
+                },
+                "pics/animations/doors.png": {
+                    tile: 120,
+                    tileh: 120,
+                    map: {
+                        dClosed: [0, 0],
+                        dHalf: [0, 1],
+                        dOpen: [0, 2]
+                    }
+                },
+                "pics/animations/speakers.png": {
+                    tile: 60,
+                    tileh: 70,
+                    map: {
+                        sOff: [0, 0],
+                        s0: [0, 1],
+                        s1: [0, 2]
+                    }
+                },
+                "pics/animations/tv-dashboard.png": {
+                    tile: 60,
+                    tileh: 300,
+                    map: {
+                        tDOff: [0, 0],
+                        tDOn: [0, 1]
+                    }
+                },
+                "pics/animations/windows.png": {
+                    tile: 600,
+                    tileh: 91,
+                    map: {
+                        w0: [0, 0],
+                        w1: [0, 1],
+                        w2: [0, 2],
+                        w3: [0, 3],
+                        w4: [0, 4],
+                        w5: [0, 5],
+                        w6: [0, 6],
+                        w7: [0, 7]
+                    }
+                },
+                "pics/animations/vc.png": {
+                    tile: 60,
+                    tileh: 60,
+                    map: {
+                        vOff: [0, 0],
+                        vOn: [0, 1]
+                    }
+                }
+            }
+        }
+        Crafty.load(assets);
+    },
+
+    build_room() {
+        // button background
+        Crafty.e('2D, Canvas, buttonBg')
+            .attr({
+                x: 0,
+                y: App.size.height - 150
+            });
+
+        // Hallways
+        Crafty.e('2D, Canvas, hallwaySide')
+            .attr({
+                x: 0,
+                y: App.room.height
+            });
+        Crafty.e('2D, Canvas, hallwayUp')
+            .attr({
+                x: App.room.width,
+                y: 0
+            });
+
+        // Room walls
+        Crafty.e('2D, Canvas, Color, wall')
+            .attr({
+                x: 0,
+                y: 0
+            })
+
+        // Bed
+        components.bed = Crafty.e('2D, Canvas, Mouse, bedOff, SpriteAnimation')
+            .attr({
+                x: 20,
+                y: this.room.height / 2 - 65
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('bed');
+            });
+        components.bed.reel("bedLightOn", 2000, [
+            [0, 1]
+        ]);
+        components.bed.reel("bedLightOff", 2000, [
+            [0, 0]
+        ]);
+
+        // Desk
+        Crafty.e('2D, Canvas, desk')
+            .attr({
+                x: 25,
+                y: 165   // 10 out from bed
+            });
+
+        // Desk with voice-assistant
+        components.voiceAssistant = Crafty.e('2D, Canvas, vOff, Mouse, SpriteAnimation')
+            .attr({
+                x: 25,
+                y: 375   // 10 out from bed
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('voice-assistant');
+            });
+        components.voiceAssistant.reel("listening", 6000, [
+            [0, 1],
+            [0, 0]
+        ]);
+
+        // A/C
+        components.airConditioning = Crafty.e('2D, Canvas, acOff, Mouse, SpriteAnimation')
+            .attr({
+                x: 20,
+                y: 490
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('ac');
+            });
+        components.airConditioning.reel("blowing", 2000, [
+            [0, 1],
+            [0, 2],
+            [0, 1],
+            [0, 2],
+            [0, 1],
+            [0, 2],
+            [0, 1],
+            [0, 2],
+            [0, 0]
+        ]);
+
+        // Window
+        components.window = Crafty.e('2D, Canvas, w0, Mouse, SpriteAnimation')
+            .attr({
+                x: 50,
+                y: 0
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('window');
+            });
+        components.window.reel("wClosing", 2000, [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4],
+            [0, 5],
+            [0, 6]
+        ]);
+        components.window.reel("wOpening", 1000, [
+            [0, 0],
+            [0, 7]
+        ]);
+
+        // Chairs
+        Crafty.e('2D, Canvas, chair')
+            .attr({
+                x: 200,
+                y: 70
+            });
+        Crafty.e('2D, Canvas, chair')
+            .attr({
+                x: 360,
+                y: 70
+            });
+
+        // TV/Dashboard
+        components.TV = Crafty.e('2D, Canvas, tDOff, Mouse, SpriteAnimation, Sprite')
+            .attr({
+                x: 660,
+                y: this.room.height / 2 - 150
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('dashboard');
+            });
+        components.TV.reel("dashboardAlert", 500, [
+            [0, 0],
+            [0, 1]
+        ]);
+
+        // Door
+        components.door = Crafty.e('2D, Canvas, dClosed, SpriteAnimation')
+            .attr({
+                x: 500,
+                y: 480
+            });
+        components.door.reel("dOpening", 1000, [
+            [0, 0],
+            [0, 1],
+            [0, 2]
+        ]);
+        components.door.reel("dClosing", 1000, [
+            [0, 2],
+            [0, 1],
+            [0, 0]
+        ]);
+
+        // Speakers
+        components.speaker1 = Crafty.e('2D, Canvas, sOff, Mouse, SpriteAnimation')
+            .attr({
+                x: 620,
+                y: 60
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('speaker');
+            });
+        components.speaker1.reel("music", 2000, [
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 0]
+        ]);
+        components.speaker2 = Crafty.e('2D, Canvas, sOff, Mouse, SpriteAnimation')
+            .attr({
+                x: 620,
+                y: 470
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('speaker');
+            });
+        components.speaker2.reel("music", 2000, [
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 0]
+        ]);
+
+        // Nurse
+        components.nurse = Crafty.e('2D, Canvas, nurse, Mouse, Tween')
+            .attr({
+                x: App.room.width + 45,
+                y: App.room.height / 2 - 30
+            })
+            .bind('Click', function(MouseEvent) {
+                showInfoOn('nurse');
+            });
+
+    },
+
+    start: function() {
+        Crafty.init(App.size.width, App.size.height, document.getElementById('app'));
+        Crafty.viewport.scale(0.7)
+
+        App.load_sprites();
+        App.build_room();
+        window.buildButtons(window.components)
+    }
+}
---
+++ js/ui.js
@@ -0,0 +1,421 @@
+(function() {
+	const getInfoOn = (object) => {
+		switch(object) {
+		case "bed":
+			return "The bed is electric and contains a temperature regulating mattress to ensure the patient remains at a comfortable temperature."
+		case "voice-assistant":
+			return "The patient is able to ask questions, alter their room conditions, or call for the nurse with specific requests."
+		case "ac":
+			return "Air-conditioning is available to regulate room temperature based on the patient's temperature."
+		case "window":
+			return "The automatic windows open and close to ensure fresh air enters the room and maintain temperature levels. Also, the blinds automatically open and close."
+		case "dashboard":
+			return "All results from the sensors connected to the patient are linked to a dashboard and displayed clearly. The dashboard will alert a nurse if patient levels become abnormal. This makes it easier for nurses to manage their patients. A TV is shown on the patient's side for their entertainment."
+		case "speaker":
+			return "Appropriate music is played when the patient is reading or sleeping to aid in these activities."
+		case "nurse":
+			return "The nurse is able to manage all their patients in one place from the dashboard."
+		}
+	}
+
+	window.showInfoOn = (object) => {
+		const title = object
+			  .split("-")
+			  .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
+			  .join(" ")
+		const textBox = document.querySelector("#textBox")
+		textBox.style = ""
+		textBox.innerHTML = `
+<h1>${title}</h1>
+<p class="content">${getInfoOn(object)}</p>
+`
+	}
+
+	function getCaption(action) {
+		switch(action) {
+		case "temp":
+			return "The patient's temperature is irregular. The air-conditioning is turned on to adjust the room temperature. Also, the windows may be opened. The bed heaters may be adjusted too."
+		case "sleep":
+			return "To aid in sleeping, the blinds are closed and the lights switched off. Furthermore, soothing music is played."
+		case "heart":
+			return "If the patient enters a dangerous state, the data fed into the patient dashboard is updated which alerts a nurse to attend the patient."
+		case "speech":
+			return "The patient can make commands and call for the nurse using the voice assistant."
+		case "reading":
+			return "When reading, a light above the patient is switched on and a reading music soundtrack is played."
+		case "medicine":
+			return "The patient dashboard monitors patient levels and automatically regulates medicine intake. If the medicine needs to be given by a nurse, they are alerted."
+		}
+	}
+
+	const captionThis = (action) => {
+		const caption = document.querySelector("#caption")
+		caption.style = ""
+		caption.innerHTML = `<p>${getCaption(action)}</p>`
+	}
+
+	window.buildButtons = (components) => {
+		// Temperature
+		Crafty.e('2D, Canvas, tempButton, Mouse')
+			.attr({
+				x: 46,
+				y: App.size.height - 114
+			})
+			.bind('Click', function(MouseEvent) {
+				// Show temperature icon above patient
+				captionThis('temp');
+				Crafty.e('2D, Canvas, Tween, temp')
+					.attr({
+						x: 90,
+						y: 200
+					})
+				// Show temperature icon above patient
+					.tween({
+						y: 150
+					}, 200)
+					.one("TweenEnd", function() {
+						this.tween({
+							alpha: 1.0
+						}, 2000);
+						components.airConditioning.animate("blowing", 1);
+						components.window.animate("wOpening", 1);
+						this.one("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0,
+							}, 500);
+						});
+					});
+			});
+
+		// Sleeping
+		Crafty.e('2D, Canvas, sleepButton, Mouse')
+			.attr({
+				x: 141,
+				y: App.size.height - 114
+			})
+			.bind('Click', function(MouseEvent) {
+				captionThis('sleep');
+				// Show temperature icon above patient
+				Crafty.e('2D, Canvas, Tween, sleep')
+					.attr({
+						x: 90,
+						y: 200
+					})
+				// Show temperature icon above patient
+					.tween({
+						y: 150
+					}, 200)
+					.one("TweenEnd", function() {
+						this.tween({
+							alpha: 1.0
+						}, 2000);
+						components.bed.animate("bedLightOff", 1);
+						components.window.animate("wClosing", 1);
+						components.speaker1.animate("music", 1);
+						components.speaker2.animate("music", 1);
+						this.one("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0,
+							}, 500);
+						});
+					});
+			});
+
+		// Heart
+		Crafty.e('2D, Canvas, heartButton, Mouse')
+			.attr({
+				x: 236,
+				y: App.size.height - 114
+			})
+			.bind('Click', function(MouseEvent) {
+				captionThis('heart');
+				Crafty.e('2D, Canvas, Tween, heart')
+					.attr({
+						x: 90,
+						y: 200
+					})
+					.tween({
+						y: 150
+					}, 200)
+					.one("TweenEnd", function() {
+						this.tween({
+							alpha: 1.0
+						}, 2000);
+						this.one("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0,
+							}, 500);
+						});
+					});
+				Crafty.e('2D, Canvas, Tween, alert')
+					.attr({
+						x: 711,
+						y: 150
+					})
+					.tween({
+						y: 130
+					}, 200)
+					.one("TweenEnd", function() {
+						components.TV.animate("dashboardAlert", 4);
+						this.tween({
+							alpha: 1.0
+						}, 3000).one("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0
+							}, 500);
+						});
+					});
+
+				Crafty.e("Delay").delay(function() {
+					components.nurse.tween({
+						y: 635
+					}, 1000).one("TweenEnd", function() {
+						Crafty.e("Delay").delay(function() {
+							// open door when nurse arrives
+							components.door.animate("dOpening", 1);
+							components.nurse.pauseTweens();
+							Crafty.e("Delay").delay(function() {
+								// wait for door to open
+								components.nurse.resumeTweens();
+							}, 1200, 0);
+						}, 500, 0);
+						this.tween({
+							x: 520
+						}, 500).one("TweenEnd", function() {
+							this.tween({
+								y: 400
+							}, 600).one("TweenEnd", function() {
+								Crafty.e("Delay").delay(function() {
+									// attend patient
+									Crafty.e('2D, Canvas, Tween, good')
+										.attr({
+											x: 90,
+											y: 200
+										})
+									// Show temperature icon above patient
+										.tween({
+											y: 150
+										}, 200).one("TweenEnd", function() {
+											components.TV.sprite("tDOff");
+											this.tween({
+												alpha: 1.0
+											}, 2000);
+											this.one("TweenEnd", function() {
+												this.tween({
+													alpha: 0.0,
+												}, 500);
+											});
+										});
+									components.nurse.pauseTweens();
+								}, 1000, 0);
+								this.tween({
+									x: 150
+								}, 1000).one("TweenEnd", function() {
+									Crafty.e("Delay").delay(function() {
+										// stop attending patient
+										components.nurse.resumeTweens();
+									}, 1500, 0);
+									// returning
+									Crafty.e("Delay").delay(function() {
+										// wait for door to close
+										components.door.animate("dClosing", 1);
+										components.nurse.pauseTweens();
+									}, 3100, 0);
+									this.tween({
+										x: 520
+									}, 1000).one("TweenEnd", function() {
+										Crafty.e("Delay").delay(function() {
+											// stop attending patient
+											components.nurse.resumeTweens();
+										}, 1600, 0);
+										this.tween({
+											y: 635
+										}, 600).one("TweenEnd", function() {
+											this.tween({
+												x: 745
+											}, 500).one("TweenEnd", function() {
+												this.tween({
+													y: 270
+												}, 1000);
+											});
+										});
+									});
+								});
+							});
+						});
+					});
+				}, 2000, 0);
+			});
+
+		// Voice assistant
+		Crafty.e('2D, Canvas, speechButton, Mouse')
+			.attr({
+				x: 331,
+				y: App.size.height - 114
+			})
+			.bind('Click', function(MouseEvent) {
+				captionThis('speech');
+				// Show temperature icon above patient
+				Crafty.e('2D, Canvas, Tween, speech')
+					.attr({
+						x: 90,
+						y: 200
+					})
+				// Show temperature icon above patient
+					.tween({
+						y: 150
+					}, 200).one("TweenEnd", function() {
+						this.tween({
+							alpha: 1.0
+						}, 2000);
+						components.voiceAssistant.animate("listening", 1);
+						this.one("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0,
+							}, 500);
+						});
+					});
+
+			});
+
+		// Reading
+		Crafty.e('2D, Canvas, bookButton, Mouse')
+			.attr({
+				x: 426,
+				y: App.size.height - 114
+			})
+			.bind('Click', function(MouseEvent) {
+				captionThis('reading');
+				// Show temperature icon above patient
+				Crafty.e('2D, Canvas, Tween, reading')
+					.attr({
+						x: 90,
+						y: 200
+					})
+				// Show temperature icon above patient
+					.tween({
+						y: 150
+					}, 200).one("TweenEnd", function() {
+						this.tween({
+							alpha: 1.0
+						}, 2000);
+						components.bed.animate("bedLightOn", 1);
+						components.speaker1.animate("music", 1);
+						components.speaker2.animate("music", 1);
+						this.one("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0,
+							}, 500);
+						});
+					});
+			});
+
+		// Medicine
+		Crafty.e('2D, Canvas, medicineButton, Mouse')
+			.attr({
+				x: 521,
+				y: App.size.height - 114
+			})
+			.bind('Click', function(MouseEvent) {
+				captionThis('medicine');
+				Crafty.e('2D, Canvas, Tween, medicine')
+					.attr({
+						x: 711,
+						y: 150
+					})
+
+					.tween({
+						y: 130
+					}, 200).bind("TweenEnd", function() {
+						this.unbind("TweenEnd");
+						components.TV.animate("dashboardAlert", 4);
+						this.tween({
+							alpha: 1.0
+						}, 3000).bind("TweenEnd", function() {
+							this.tween({
+								alpha: 0.0
+							}, 500);
+						});
+					});
+				Crafty.e("Delay").delay(function() {
+					components.nurse.tween({
+						y: 635
+					}, 1000).one("TweenEnd", function() {
+						Crafty.e("Delay").delay(function() {
+							// open door when nurse arrives
+							components.door.animate("dOpening", 1);
+							components.nurse.pauseTweens();
+							Crafty.e("Delay").delay(function() {
+								// wait for door to open
+								components.nurse.resumeTweens();
+							}, 1200, 0);
+						}, 500, 0);
+						this.tween({
+							x: 520
+						}, 500).one("TweenEnd", function() {
+							this.tween({
+								y: 400
+							}, 600).one("TweenEnd", function() {
+								Crafty.e("Delay").delay(function() {
+									// attend patient
+									Crafty.e('2D, Canvas, Tween, good')
+										.attr({
+											x: 90,
+											y: 200
+										})
+									// Show temperature icon above patient
+										.tween({
+											y: 150
+										}, 200).one("TweenEnd", function() {
+											components.TV.sprite("tDOff");
+											this.tween({
+												alpha: 1.0
+											}, 2000);
+											this.one("TweenEnd", function() {
+												this.tween({
+													alpha: 0.0,
+												}, 500);
+											});
+										});
+									components.nurse.pauseTweens();
+								}, 1000, 0);
+								this.tween({
+									x: 150
+								}, 1000).one("TweenEnd", function() {
+									Crafty.e("Delay").delay(function() {
+										// stop attending patient
+										components.nurse.resumeTweens();
+									}, 1500, 0);
+									// returning
+									Crafty.e("Delay").delay(function() {
+										// wait for door to close
+										components.door.animate("dClosing", 1);
+										components.nurse.pauseTweens();
+									}, 3100, 0);
+									this.tween({
+										x: 520
+									}, 1000).one("TweenEnd", function() {
+										Crafty.e("Delay").delay(function() {
+											// stop attending patient
+											components.nurse.resumeTweens();
+										}, 1600, 0);
+										this.tween({
+											y: 635
+										}, 600).one("TweenEnd", function() {
+											this.tween({
+												x: 745
+											}, 500).one("TweenEnd", function() {
+												this.tween({
+													y: 270
+												}, 1000);
+											});
+										});
+									});
+								});
+							});
+						});
+					});
+				}, 2000, 0);
+			});
+	}
+}())
